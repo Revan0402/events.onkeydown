64a16d5ecb3832d4969c4910097e3ee0ba1a5c4e
Revan0402 <ralizade198@gmail.com>
2022-10-18 11:37:33 +0400

    update

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+//butun parametrler
+
+/*onkeydown = function(r) {
+    console.log(r)
+}*/
+
+
+
+//sadece herf
+
+/*onkeydown = function(r) {
+    console.log(r.key)
+}*/
+
+
+//sira nomreleri
+
+/*onkeydown = function(r) {
+    console.log(r.key + "-" + r.keyCode)
+}*/
+
+
+
+/*onkeydown = function(r) {
+    if(r.keyCode >=48 && r.keyCode <=57){
+        this.alert("reqem daxil etmek olmaz")
+    }else{
+        console.log(r.key + "-" + r.keyCode)
+    }
+}*/
